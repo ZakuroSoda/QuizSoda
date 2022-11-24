5cb7560f36cc2159a597ac3ccf273fe162068f74
Reyes <21reyes.lee.yui.hou@acsians.acsi.edu.sg>
2022-11-24 13:41:28 +0800

    authentication almost fully working

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', navBarPage="home")
+    session = SessionManager('database.db')
+    check = session.get_session(request.cookies)
+    if check == None: #if not logged in or if sessionID is wrong
+        return render_template('index.html', navBarPage="home", authenticated=False)
+    else: # if logged in
+        return render_template('index.html', navBarPage="home", authenticated=True)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -15,7 +20,7 @@ def login():
         session = SessionManager('database.db')
         check = session.get_session(request.cookies)
         if check == None: #if not logged in or if sessionID is wrong
-            resp = make_response(render_template('login.html', navBarPage="login"))
+            resp = make_response(render_template('login.html', navBarPage="login", authenticated=False))
             resp = session.remove_session(request.cookies, resp) 
             # clear out the session id cookie for clean login (this func also removes the token from the db but since token does not exist nothing happens)
             return resp
@@ -27,7 +32,7 @@ def login():
             username, passwordHash = request.form['username'], sha256(request.form['password'].encode('utf-8')).hexdigest()
         except Exception as e:
             print(e)
-            return render_template('login.html', navBarPage='login', message='There was an unexpected error. Please contact an admin.')
+            return render_template('login.html', navBarPage='login', message='There was an unexpected error. Please contact an admin.', authenticated=False)
         
         try:
             con = sqlite3.connect("database.db")
@@ -36,7 +41,7 @@ def login():
             results = cur.fetchall()
 
             if len(results) == 0:
-                return render_template('login.html', navBarPage='login', message='Wrong username or password.')
+                return render_template('login.html', navBarPage='login', message='Wrong username or password.', authenticated=False)
 
             elif len(results) == 1 and results[0][0] == username:
                 # logged in (set as authenticated and return to home)
@@ -47,7 +52,7 @@ def login():
 
         except Exception as e:
             print(e)
-            return render_template('login.html', navBarPage='login', message='There was an unexpected error. Please contact an admin.')
+            return render_template('login.html', navBarPage='login', message='There was an unexpected error. Please contact an admin.', authenticated=False)
 
     
 @app.route('/register', methods=['GET','POST'])
@@ -56,7 +61,7 @@ def register():
         session = SessionManager('database.db')
         check = session.get_session(request.cookies)
         if check == None: #if not logged in or if sessionID is wrong
-            resp = make_response(render_template('register.html', navBarPage="register"))
+            resp = make_response(render_template('register.html', navBarPage="register", authenticated=False))
             resp = session.remove_session(request.cookies, resp) 
             # clear out the session id cookie for clean register (this func also removes the token from the db but since token does not exist nothing happens)
             return resp
@@ -67,7 +72,7 @@ def register():
         # since this application is meant to be small I really don't mind slapping all the validation here
         username, password, passwordConfirm = request.form['username'], request.form['password'], request.form['passwordConfirm']
         if password != passwordConfirm:
-            return render_template('register.html', navBarPage='register', message='Your passwords do not match. Please try again.')
+            return render_template('register.html', navBarPage='register', message='Your passwords do not match. Please try again.', authenticated=False)
         
         # check for username already exists
         try:
@@ -75,17 +80,17 @@ def register():
             cur = con.cursor()
             cur.execute("SELECT username FROM users WHERE username=?", (username, ))
             if len(cur.fetchall()) != 0:
-                return render_template('register.html', navBarPage='register', message='Invalid username.')
+                return render_template('register.html', navBarPage='register', message='Invalid username.', authenticated=False)
 
         except Exception as e:
             print(e)
-            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.')
+            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.', authenticated=False)
         
         try:
             passwordHash = sha256(password.encode('utf-8')).hexdigest()
         except Exception as e:
             print(e)
-            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.')
+            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.', authenticated=False)
         
         try:
             con = sqlite3.connect("database.db")
@@ -95,7 +100,7 @@ def register():
 
         except Exception as e:
             print(e)
-            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.')
+            return render_template('register.html', navBarPage='register', message='There was an unexpected error. Please contact an admin.', authenticated=False)
         
         # successfully registered! (set as authenticated and redirect to home)
         session = SessionManager('database.db')
@@ -105,6 +110,11 @@ def register():
 
 @app.route('/challenges')
 def challenge():
-    return render_template('challengeGallery.html', navBarPage="challenges")
+    session = SessionManager('database.db')
+    check = session.get_session(request.cookies)
+    if check == None: #if not logged in or if sessionID is wrong
+        return redirect(url_for('login'))
+    else:
+        return render_template('challengeGallery.html', navBarPage="challenges", authenticated=True)
 
 app.run(port=5000, host='0.0.0.0', debug=True)

047f443f54b8800ff9315e180bfb289d2ffc472f
Reyes Lee <87415308+ZakuroSoda@users.noreply.github.com>
2022-12-17 10:31:31 +0800

    Made Proper Challenges

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def leaderboard():
         return render_template('leaderboard.html', navBarPage="leaderboard", authenticated=True, username=loggedIn, leaderboard=leaderboard)
 
 if __name__ == '__main__':
-    # run resetAll() to reset everything
+    resetAll()
     app.run(port=5000, host='0.0.0.0', debug=True)
